8da6f9be31a7260adab2acecec1c4d2139cdb2bb
mpjashby <mpjashby@gmail.com>
2022-04-24 19:25:31 +0100

    Add previous reports

--- code/01_download_data.R
+++ code/01_download_data.R
@@ -3,24 +3,44 @@
 # file structure of CSV files, one for each force each month.
 
 # load packages
-library("osmdata")
-library("tidyverse")
+library(httr)
+library(lubridate)
+library(osmdata)
+library(tidyverse)
 
 
 
-# GET SEARCH DATA
+# GET SEARCH DATA --------------------------------------------------------------
 
-# unzip data into a temporary directory
-unzip(here::here("original-data/7d23b916fcbeff6e8f8b0c6a8ec560b4e245c886.zip"),
-      exdir = str_glue("{tempdir()}/stop-data/"))
-
-str_glue("{tempdir()}/stop-data") %>% 
-  dir(full.names = TRUE, recursive = TRUE) %>% 
+data_dir <- tempdir()
+data_file <- str_glue("{data_dir}/stop_data.zip")
+GET(
+  "https://data.police.uk/data/archive/2021-12.zip", 
+  write_disk(data_file), 
+  progress()
+)
+data_files <- data_file %>% 
+  unzip(list = TRUE) %>% 
+  as_tibble() %>% 
+  filter(str_detect(Name, "stop-and-search.csv$")) %>% 
+  pull("Name")
+unzip(data_file, files = data_files, exdir = str_glue("{data_dir}"))
+data_dir %>% 
+  dir(pattern = "stop-and-search.csv$", full.names = TRUE, recursive = TRUE) %>% 
   set_names() %>% 
-  map_dfr(read_csv, .id = "force") %>% 
+  map_dfr(
+    read_csv, 
+    col_types = cols(
+      .default = col_character(),
+      Date = col_datetime(format = ""),
+      Latitude = col_double(),
+      Longitude = col_double()
+    ), 
+    .id = "force"
+  ) %>% 
   janitor::clean_names() %>% 
-  mutate_at(vars(gender, object_of_search, outcome), str_to_lower) %>% 
   mutate(
+    across(.cols = c(gender, object_of_search, outcome), .fns = str_to_lower),
     force = case_when(
       str_detect(force, "btp") ~ "British Transport Police",
       str_detect(force, "city-of-london") ~ "City of London Police",
@@ -28,11 +48,18 @@ str_glue("{tempdir()}/stop-data") %>%
       TRUE ~ NA_character_
     )
   ) %>% 
+  filter(!is.na(force)) %>% 
+  select(
+    -part_of_a_policing_operation, 
+    -policing_operation, 
+    -outcome_linked_to_object_of_search,
+    -removal_of_more_than_just_outer_clothing
+  ) %>% 
   write_rds(here::here("analysis-data/stops.rds"))
 
 
 
-# GET POPULATION DATA
+# GET POPULATION DATA ----------------------------------------------------------
 
 # data is from https://data.london.gov.uk/dataset/ethnic-group-population-projections
 download.file(
@@ -87,8 +114,58 @@ map_dfr(
   write_rds(here::here("analysis-data/people.rds"))
 
 
+## Borough-level population data ----
 
-# GET HISTORICAL STOPS DATA
+map_dfr(
+  c("Population - Females", "Population - Males"), 
+  ~ readxl::read_excel(path = here::here("original-data/population_data.xlsx"), 
+                       sheet = .)
+) %>% 
+  filter(
+    age != "All ages", 
+    !borough %in% c("Inner London", "Outer London"), 
+    !ethnic_group %in% c("All persons", "BAME")
+  ) %>% 
+  mutate(
+    age = as.numeric(age),
+    age_range = case_when(
+      between(age, 0, 9) ~ "under 10",
+      between(age, 10, 17) ~ "10-17",
+      between(age, 18, 24) ~ "18-24",
+      between(age, 25, 34) ~ "25-34",
+      age > 34 ~ "over 34",
+      TRUE ~ NA_character_
+    ),
+    borough = recode(borough, "Greater London" = "London"),
+    self_defined_ethnicity = recode(
+      ethnic_group,
+      "White British" = "White - English/Welsh/Scottish/Northern Irish/British",
+      "White Irish" = "White - Irish",
+      "Other White" = "White - Any other White background",
+      "Black African" = "Black/African/Caribbean/Black British - African",
+      "Black Caribbean" = "Black/African/Caribbean/Black British - Caribbean",
+      "Other Black" = "Black/African/Caribbean/Black British - Any other Black/African/Caribbean background",
+      "White & Black Caribbean" = "Mixed/Multiple ethnic groups - White and Black Caribbean",
+      "White & Black African" = "Mixed/Multiple ethnic groups - White and Black African",
+      "White & Asian" = "Mixed/Multiple ethnic groups - White and Asian",
+      "Other Mixed" = "Mixed/Multiple ethnic groups - Any other Mixed/Multiple ethnic background",
+      "Indian" = "Asian/Asian British - Indian",
+      "Pakistani" = "Asian/Asian British - Pakistani",
+      "Bangladeshi" = "Asian/Asian British - Bangladeshi",
+      "Other Asian" = "Asian/Asian British - Any other Asian background",
+      "Chinese" = "Asian/Asian British - Chinese",
+      "Arab" = "Other ethnic group - Any other ethnic group",
+      "Other Ethnic Group" = "Other ethnic group - Any other ethnic group"
+    ),
+    sex = str_to_lower(sex)
+  ) %>% 
+  rename(people = `2020`) %>% 
+  count(borough, sex, age_range, self_defined_ethnicity, wt = people, name = "people") %>% 
+  write_rds(here::here("analysis-data/people-by-borough.rds"))
+
+
+
+# GET HISTORICAL STOPS DATA ----------------------------------------------------
 
 download.file(
   url = "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/841261/stop-search-open-data-tables-ppp.ods",
@@ -116,7 +193,8 @@ hist_data <- here::here("original-data/historical_stops_data.ods") %>%
   write_rds(here::here("analysis-data/historical-stop-data.rds"))
 
 
-# GET DEPRIVATION
+
+# GET DEPRIVATION --------------------------------------------------------------
 
 download.file(
   url = "https://data.london.gov.uk/download/indices-of-deprivation/9ee0cf66-e6f9-4e38-8eec-79c1d897e248/ID%202019%20for%20London.xlsx",
@@ -138,46 +216,53 @@ here::here("original-data/deprivation_data.xlsx") %>%
 
 
 
-# GET STATIONS DATA
+# GET STATIONS DATA ------------------------------------------------------------
 
-getbb("London, UK") %>% 
+stations_data <- getbb("London, UK") %>% 
   opq() %>% 
   add_osm_feature(key = "railway", value = "station") %>% 
   osmdata_sf() %>% 
   pluck("osm_points") %>% 
-  select(name, geometry) %>% 
-  filter(!str_detect(name, "^Gates ")) %>% 
-  mutate(name = recode(
-    str_squish(str_remove(
-      str_remove(
-        str_remove(
-          str_remove(
-            str_remove(
-              str_remove(
-                str_remove(
-                  str_remove(name, "Thameslink$"), 
-                  "^London "
-                ), 
-                " (Central|East|North|South|West)$"
-              ), 
-              " Terminal .+?$"
-            ), 
-            " DLR$"
-          ), 
-          " Station$"
-        ), 
-        " for .+?$"
-      ), 
-      "\\b\\(+?\\)\\b"
-    )),
-    "Caledonian Road & Barnsbury" = "Caledonian Road",
-    "East Croydon" = "Croydon Town Centre",
-    "West Croydon" = "Croydon Town Centre",
-    "Finchley Road & Frognal" = "Finchley Road",
-    "King's Cross St Pancras" = "King's Cross",
-    "Bridge" = "London Bridge",
-    "St. Pancras International" = "King's Cross",
-    "Stratford International" = "Stratford",
-    "Maryland" = "Stratford"
-  )) %>% 
+  select(old_name = name, geometry) %>% 
+  filter(!str_detect(old_name, "^Gates ")) %>% 
+  mutate(
+    name = old_name %>% 
+      str_remove(" Thameslink$") %>% 
+      str_remove("^London ") %>% 
+      str_remove(" (Central|East|North|South|West)$") %>% 
+      str_remove(" Terminal .+?$") %>% 
+      str_remove(" DLR$") %>% 
+      str_remove(" Station$") %>% 
+      str_remove(" for .+?$") %>% 
+      str_remove("\\b\\(+?\\)\\b") %>% 
+      str_squish() %>% 
+      recode(
+        "Abbey Road" = "West Ham",
+        "Acton Main Line" = "Acton",
+        "Bridge" = "London Bridge",
+        "Caledonian Road & Barnsbury" = "Caledonian Road",
+        "East Croydon" = "Croydon Town Centre",
+        "Edgware Road (Bakerloo line)" = "Edgware Road",
+        "Edgware Road (Circle, District and Hammersmith & City lines)" = "Edgware Road",
+        "Finchley Road & Frognal" = "Finchley Road",
+        "Green Park" = "Piccadilly",
+        "King's Cross St Pancras" = "King's Cross",
+        "Maryland" = "Stratford",
+        "Piccadilly Circus" = "Piccadilly",
+        "Russell Square" = "Bloomsbury",
+        "St. Pancras International" = "King's Cross",
+        "Shoreditch High Street" = "Shoreditch",
+        "Stratford International" = "Stratford",
+        "Tower Gateway" = "Tower Hill",
+        "West Croydon" = "Croydon Town Centre",
+        "Willesden Junction" = "Harlesden"
+      ),
+    name = case_when(
+      old_name %in% c("Clapham Common", "Clapham High Street", "Clapham North") ~ 
+        "Clapham High St",
+      old_name %in% c("Peckham Rye", "Queens Road Peckham") ~ "Peckham",
+      TRUE ~ name
+    )
+  ) %>% 
+  select(name) %>%
   st_write(here::here("analysis-data/stations.gpkg"))
